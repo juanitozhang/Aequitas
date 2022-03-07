43560f5959c411fb5e6f706bb1d65cb74bb5a90d
Yunping Wang <yunpingwang@Yunpings-MacBook-Air.local>
2022-03-07 15:56:56 -0600

    Added documentation for fairness estimation

--- Phemus/src/Phemus/Sklearn_Estimation.py
+++ Phemus/src/Phemus/Sklearn_Estimation.py
@@ -61,6 +61,18 @@ def get_estimate_array(dataset: Dataset, model, threshold, num_trials, samples):
     return estimate_array
 
 def get_fairness_estimation(dataset: Dataset, input_pkl_name, threshold, num_trials, samples):
+    """
+    Takes a trained model and estimate its fairness. Return the final fairness estimation on a
+    normalized 0-1 scale.
+
+    Keyword arguments:
+    dataset -- A dataset object that contains the meta information about the dataset used
+    input_pkl_dir -- The directory where the input model in .pkl formate is located
+    threshold -- A quantitative threshold used to determine whether a input is discrimitory or not. 
+                 For a binary classification, this threshold should be zero.
+    num_trials -- The number of trials that will be conducted when doing fairness estimation
+    samples -- The number of samples that will be taken when doing fairness estimation
+    """
     model = joblib.load(input_pkl_name)
     arr = get_estimate_array(dataset, model, threshold, num_trials, samples)
     print("final biasedness estimate: " + str(np.mean(arr) * 100))
